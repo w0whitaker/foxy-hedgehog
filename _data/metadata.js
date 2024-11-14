89d5ee019d97544230d366f3b2fb11eafb624b7d
William Whitaker <william@williamwhitaker.dev>
2024-11-13 20:12:13 -0500

    update metadata and layouts

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Foxy Hedgehog",
+	url: "https://foxy-hedgehog.net/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "One big many things.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "William Whitaker",
+		email: "williamwhitaker@fastmail.com",
+		url: "https://foxy-hedgehog/about-me/",
+	},
+};
