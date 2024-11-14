917615c40a77d165e0856fd27376c40b19089a84
William Whitaker <william@williamwhitaker.dev>
2024-11-14 12:18:44 -0500

    update metadata and layouts

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "Foxy Hedgehog",
 	url: "https://foxy-hedgehog.net/",
 	language: "en",
-	description: "One big many things.",
+	description: "Many one big things.",
 	author: {
 		name: "William Whitaker",
 		email: "williamwhitaker@fastmail.com",
